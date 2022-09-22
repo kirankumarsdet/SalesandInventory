cce299c8b86cf32c3db573f16ade3adb4bf3d4c2
kirankumarsdet <113989443+kirankumarsdet@users.noreply.github.com>
2022-09-22 15:42:18 +0530

    Update AjioTOexcel.java

--- src/test/java/org/altairretro/accounts/AjioTOexcel.java
+++ src/test/java/org/altairretro/accounts/AjioTOexcel.java
@@ -24,6 +24,7 @@ public class AjioTOexcel {
 
 	public static void main(String[] args) throws EncryptedDocumentException, IOException {
 
+		//update
 		WebDriverManager.chromedriver().setup();
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--disable-notifications");

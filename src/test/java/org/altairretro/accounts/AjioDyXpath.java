af7921de6fff9c75256d2559c4ccb6d8ca438823
91910 <91910@DESKTOP-E5FOM8C>
2022-09-23 13:32:35 +0530

    Ajioupdate

--- src/test/java/org/altairretro/accounts/AjioDyXpath.java
+++ src/test/java/org/altairretro/accounts/AjioDyXpath.java
@@ -17,21 +17,26 @@ public class AjioDyXpath {
 
 	public static void main(String[] args) throws EncryptedDocumentException, IOException {
 
-		WebDriverManager.chromedriver().setup();
-		ChromeOptions options = new ChromeOptions();
-		options.addArguments("--disable-notifications");
-		WebDriver driver = new ChromeDriver(options);
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-		driver.get("https://www.ajio.com");
-		driver.findElement(By.xpath("//a[text()='KIDS']")).click();
-		driver.findElement(By.xpath("//a[@href=\"/s/0-to-2-years-3767-54091\"]")).click();
-		driver.findElement(By.xpath("//div[text()='AARIKA GIRLS ETHNIC']"));
-		String productprice = driver.findElement(By.xpath(
-				"//a[@href='/aarika-girls-ethnic-indian-fit-and-flare-dress/p/463654377_black']/descendant::span[@class='price  ']"))
-				.getText();
-		System.out.println(productprice);
+//		WebDriverManager.chromedriver().setup();
+//		ChromeOptions options = new ChromeOptions();
+//		options.addArguments("--disable-notifications");
+//		WebDriver driver = new ChromeDriver(options);
+//		driver.manage().window().maximize();
+//		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//
+//		driver.get("https://www.ajio.com");
+//		driver.findElement(By.xpath("//a[text()='KIDS']")).click();
+//		driver.findElement(By.xpath("//a[@href=\"/s/0-to-2-years-3767-54091\"]")).click();
+//		driver.findElement(By.xpath("//div[text()='AARIKA GIRLS ETHNIC']"));
+//		String productprice = driver.findElement(By.xpath(
+//				"//a[@href='/aarika-girls-ethnic-indian-fit-and-flare-dress/p/463654377_black']/descendant::span[@class='price  ']"))
+//				.getText();
+//		System.out.println(productprice);
+		
+		String browser= System.getProperty("br");
+		String url=System.getProperty("ur");
+		System.out.println(browser);
+		System.out.println(url);
 
 	}
 }
